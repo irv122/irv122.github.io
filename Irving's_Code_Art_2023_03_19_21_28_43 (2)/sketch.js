40b12053d8f1f3474f4ef5ac0074a96bfc50f6d3
irv122 <118488609+irv122@users.noreply.github.com>
2023-03-26 08:44:38 -0400

    Add files via upload

---
+++ Irving's_Code_Art_2023_03_19_21_28_43 (2)/sketch.js
@@ -0,0 +1,35 @@
+function setup() {
+  angleMode(DEGREES);
+  rectMode(CENTER);
+  createCanvas(400, 400);
+  background(70, 135, 83,150);
+  fill(0); //black hair
+  fill(179, 40, 21); //teal
+  rect(200,380,190,270,40); //shirt
+  fill(189, 102, 47); //light brown
+  rect(200,239,100,100,30); //neck
+  ellipse(200,150,150,175); //face
+  fill(0); //black hair
+  rect(200,75,165,70,90); //hair
+  fill(255); //white
+  ellipse(170,143,50,40); //left eye
+  ellipse(230,143,50,40); //right eye
+  fill(0); //black
+  ellipse(170,143,22,25); //left eye
+  ellipse(230,143,22,25); //right eye
+  fill(82,72,156);
+  arc(200,192,50,50,0,180); //mouth
+  noFill();
+  arc(198,175,25,15,270,90); //nose
+  strokeWeight(5)
+  stroke(0); //black
+  line(150,115,175,115); //left eyebrow
+  line(225,115,250,115); //right eyebrow
+  strokeWeight(1);
+  stroke(0)
+}
+
+
+
+
+
