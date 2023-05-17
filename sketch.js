6fbd6631daeb398e38f85e8f5ffe8f2041013157
irv122 <118488609+irv122@users.noreply.github.com>
2023-05-16 22:10:06 -0400

    Meme for class

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var images=["hair.png"];
+var choosenImage;
+var caption=["OMG Kill Me Now"]  ;
+var pos;
+
+
+function preload(){
+ 
+  pos = floor(random(images.length));
+  
+  choosenImage =loadImage(images[pos]);
+  
+}
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background(220);
+  image(choosenImage,0,0,width,height);
+  textSize(50);
+ text(caption[pos], 00,60);
+}
+function mousePressed() {
+  pos = floor(random(3))
+    
+  
+}
